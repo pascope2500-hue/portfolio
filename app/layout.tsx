db7bcf8470a788f23712a6cad918063ac9f216fc
pascal <pascope2500@gmail.com>
2025-08-31 00:16:03 +0200

    correct site key

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,34 @@
-import type React from "react"
-import "@/app/globals.css"
-import { Inter } from "next/font/google"
-import { ThemeProvider } from "@/components/theme-provider"
-import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3"
+import type React from "react";
+import "@/app/globals.css";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import ReCaptchaProvider from "@/components/ReCaptchaProvider";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Pascal Ndacyayisenga - Full Stack Web Developer Portfolio",
-  description: "Professional portfolio of Pascal Ndacyayisenga, a Full Stack Web Developer specializing in Nodejs,php, express,mariadb...",
-}
+  description:
+    "Professional portfolio of Pascal Ndacyayisenga, a Full Stack Web Developer specializing in Nodejs, php, express, mariadb...",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          <GoogleReCaptchaProvider
-          reCaptchaKey={process.env.RECAPTCHA_SITE_KEY!}
-          scriptProps={{
-            async: true,
-            defer: true,
-            appendTo: "head",
-          }}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
         >
-          {children}
-          </GoogleReCaptchaProvider>
+          <ReCaptchaProvider>{children}</ReCaptchaProvider>
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
